ba1732d926d342c8a068833c524210d98d5ab484
Isha Lavanya <isha.lavanya07@gmail.com>
2026-04-02 02:01:42 +0530

    DivisibleByFive commited

---
+++ Level_1/DivisibleByFive.java
@@ -0,0 +1,26 @@
+package Level_1;
+
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args) {
+
+        // Declare variable
+        try ( // Create scanner object for input
+                Scanner input = new Scanner(System.in)) {
+            // Declare variable
+            int number;
+            
+            // Take user input
+            System.out.print("Enter a number: ");
+            number = input.nextInt();
+            
+            // Check divisibility
+            if (number % 5 == 0) {
+                System.out.println("Is the number " + number + " divisible by 5? Yes");
+            } else {
+                System.out.println("Is the number " + number + " divisible by 5? No");
+            }
+        }
+    }
+}
